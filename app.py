7b7dfc5d877ee1df7e01111f525a384e581f5f5a
jschluger <jschluger@stuy.edu>
2016-12-02 14:03:26 -0500

    indentation swas wrong

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-        u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=z5OCLcXbxVpm5pJfALskk1aCWeBKRsNiFv8N1YYp")
+    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=z5OCLcXbxVpm5pJfALskk1aCWeBKRsNiFv8N1YYp")
     response = u.read()
     data = json.loads( response )
     return render_template("index.html", pic = data['url'] )
