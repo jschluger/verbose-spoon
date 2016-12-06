b394df0ba7ec749399c97d4328dea2f307159b22
Jordan Yaqoob <jyaqoob@stuy.edu>
2016-12-05 21:58:42 -0500

    fixed user.py

--- user.py
+++ user.py
@@ -3,12 +3,12 @@ import hashlib
 
 DATABASE = "data.db"
 
-def add_user(username, password, bday):
+def add_user(username, password):
     db = sqlite3.connect(DATABASE)
     c = db.cursor()
 
-    query = "INSERT INTO users VALUES (NULL, ?, ?, ?, ?, ?)"
-    c.execute(query, (username, hashlib.sha1(password).hexdigest(), bday, '', '',))
+    query = "INSERT INTO users VALUES (NULL, ?, ?, ?, ?)"
+    c.execute(query, (username, hashlib.sha1(password).hexdigest(), '', '',))
 
     db.commit()
     db.close()
@@ -33,35 +33,4 @@ def get_user(**kwargs):
     db.close()
     return result
 
-def get_stories(uid):
-    db = sqlite3.connect(DATABASE)
-    c = db.cursor()
-
-    query = "SELECT DISTINCT stories.storyid,stories.title from updates,stories WHERE userid = ? AND updates.storyid = stories.storyid"
-    c.execute(query, (uid,))
-
-    result = c.fetchall()
-    db.close()
-    return result if result else []
-
-def get_info(uid):
-    results = get_user(id=uid)
-    info = {
-        'uid' : results [0],
-        'username' : results[1],
-        'dob' : results[3],
-        'name' : results[4],
-        'aboutme' : results[5]
-        }
-    return info
-
-def update_profile(username, name, aboutme):
-    db = sqlite3.connect(DATABASE)
-    c = db.cursor()
-
-    query = "UPDATE users SET name = ?, aboutme = ? WHERE username = ?"
-    c.execute(query, (name, aboutme, username,))
-
-    db.commit()
-    db.close()
 
